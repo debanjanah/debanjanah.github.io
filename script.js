c04a1f49031867e191302b7699244652cb72de18
debanjanah <debanjana.h@gmail.com>
2019-02-10 13:25:01 +0530

    geh

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var me = (function() {
-  var itme = ["I'm on a quest to find the sweet spot at the intersection of design, technology and film","I believe that the best lessons of life can be learnt in antique shops","I experiment with motion design when I'm not creating montage artwork"];
+  var itme = ["I'm on a quest to find the sweet spot at the intersection of design, technology and moving media","I believe that the best lessons of life can be learnt in antique shops","I create montage artwork when I'm not experimenting with motion design."];
   var count = -1;
   return function() {
     return itme[++count % itme.length];
